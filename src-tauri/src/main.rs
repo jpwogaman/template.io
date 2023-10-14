312bb9310012c4e1558abd73387d3e245467e1e4
JP Wogaman II <jpwogaman@gmail.com>
2023-10-14 18:51:36 -0400

    deps + working on custom menu and tray items

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,82 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{
+  CustomMenuItem, 
+  Menu, 
+  Submenu, 
+  SystemTrayMenu, 
+  SystemTrayMenuItem, 
+  SystemTray, 
+  SystemTrayEvent
+};  
+use tauri::Manager;
+
 fn main() {
-  let context = tauri::generate_context!();
+  let context = tauri::generate_context!();  
+
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let close = CustomMenuItem::new("close".to_string(), "Close");
+  let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+  let about = CustomMenuItem::new("about".to_string(), "About");  
+
+  let file_submenu = Submenu::new(
+    "File", 
+    Menu::new()
+    .add_item(quit.clone())
+    .add_item(close)
+  );
+
+  let help_submenu = Submenu::new(
+    "Help", 
+    Menu::new()
+    .add_item(about)
+  );
+
+  let menu = Menu::new()
+  .add_submenu(file_submenu)
+  .add_submenu(help_submenu);
+
+  let tray_menu = SystemTrayMenu::new()
+  .add_item(quit);
+
+  let system_tray = SystemTray::new()
+  .with_menu(tray_menu);
+  
   tauri::Builder::default()  
-    .menu(tauri::Menu::os_default(&context.package_info().name))
+    .menu(menu)
+    .on_menu_event(|event| {
+      match event.menu_item_id() {
+        "quit" => {
+          std::process::exit(0);
+        }
+        "close" => {
+          event.window().close().unwrap();
+        }
+        _ => {}
+      }})
+    .system_tray(system_tray)
+    .on_system_tray_event(|app, event| {
+      match event {
+        //SystemTrayEvent::LeftClick {
+        //  position: _,
+        //  size: _,
+        //  ..
+        //} => {
+        //  system_tray.display_menu().unwrap();
+        //}
+        SystemTrayEvent::MenuItemClick { id, .. } => {
+          match id.as_str() {
+            "quit" => {
+              std::process::exit(0);
+            }
+            _ => {}
+          }
+        }
+        _ => {}
+      }}
+
+    )
     .run(context)
     .expect("error while running tauri application");
 }
@@ -15,6 +87,7 @@ fn main() {
 
 //fn main() {
 //tauri::Builder::default()
+//   .menu(tauri::Menu::os_default(&context.package_info().name))
 //   .run(tauri::generate_context!())
 //   .expect("error while running tauri application");
 //}
