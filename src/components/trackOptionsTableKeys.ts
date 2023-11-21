203183a83ce6bfb7830dac9b25a5b5f57457cbae
JP Wogaman II <jpwogaman@gmail.com>
2023-11-21 11:51:27 -0500

    dependencies, swapped order of tap/toggle tables, started working on turning the artList into a dynamic matrix similar to the MCU unit

--- src/components/trackOptionsTableKeys.ts
+++ src/components/trackOptionsTableKeys.ts
@@ -47,101 +47,6 @@ const TrackOptionsTableKeys = [
     ]
   },
   {
-    title: 'Articulations (Tap)',
-    label: 'artListTap',
-    layout: 'table',
-    keys: [
-      {
-        className: 'w-[7.5%]',
-        show: true,
-        key: 'id',
-        input: undefined,
-        selectArray: null,
-        label: 'ID'
-      },
-      {
-        className: 'w-[16.5%]',
-        show: true,
-        key: 'name',
-        input: 'text',
-        selectArray: null,
-        label: 'Name'
-      },
-      {
-        className: 'w-[00%]',
-        show: false,
-        key: 'toggle',
-        input: 'checkbox',
-        selectArray: null,
-        label: 'Toggle'
-      },
-      {
-        className: 'w-[14%]',
-        show: true,
-        key: 'codeType',
-        input: 'select',
-        selectArray: 'valAddrList',
-        label: 'Code Type'
-      },
-      {
-        className: 'w-[9.5%]',
-        show: true,
-        key: 'code',
-        input: 'select',
-        selectArray: 'valMidiList',
-        label: 'Code'
-      },
-      {
-        className: 'w-[9.5%]',
-        show: true,
-        key: 'on',
-        input: 'select',
-        selectArray: 'valMidiList',
-        label: 'On'
-      },
-      {
-        className: 'w-[00%]',
-        show: false,
-        key: 'off',
-        input: 'select',
-        selectArray: 'valMidiList',
-        label: 'Off'
-      },
-      {
-        className: 'w-[19%]',
-        show: true,
-        key: 'default',
-        input: 'checkbox',
-        selectArray: 'valDeftList',
-        label: 'Default'
-      },
-      {
-        className: 'w-[9.5%]',
-        show: true,
-        key: 'delay',
-        input: 'text',
-        selectArray: null,
-        label: 'Delay'
-      },
-      {
-        className: 'w-[9.5%]',
-        show: true,
-        key: 'changeType',
-        input: 'checkbox-switch',
-        selectArray: 'valChngList',
-        label: 'Change'
-      },
-      {
-        className: 'w-[9.5%]',
-        show: true,
-        key: 'ranges',
-        input: 'select-multiple',
-        selectArray: 'artRngsArray',
-        label: 'Ranges'
-      }
-    ]
-  },
-  {
     title: 'Articulations (Toggle)',
     label: 'artListTog',
     layout: 'table',
@@ -236,6 +141,102 @@ const TrackOptionsTableKeys = [
       }
     ]
   },
+  {
+    title: 'Articulations (Tap)',
+    label: 'artListTap',
+    layout: 'table',
+    keys: [
+      {
+        className: 'w-[7.5%]',
+        show: true,
+        key: 'id',
+        input: undefined,
+        selectArray: null,
+        label: 'ID'
+      },
+      {
+        className: 'w-[16.5%]',
+        show: true,
+        key: 'name',
+        input: 'text',
+        selectArray: null,
+        label: 'Name'
+      },
+      {
+        className: 'w-[00%]',
+        show: false,
+        key: 'toggle',
+        input: 'checkbox',
+        selectArray: null,
+        label: 'Toggle'
+      },
+      {
+        className: 'w-[14%]',
+        show: true,
+        key: 'codeType',
+        input: 'select',
+        selectArray: 'valAddrList',
+        label: 'Code Type'
+      },
+      {
+        className: 'w-[9.5%]',
+        show: true,
+        key: 'code',
+        input: 'select',
+        selectArray: 'valMidiList',
+        label: 'Code'
+      },
+      {
+        className: 'w-[9.5%]',
+        show: true,
+        key: 'on',
+        input: 'select',
+        selectArray: 'valMidiList',
+        label: 'On'
+      },
+      {
+        className: 'w-[00%]',
+        show: false,
+        key: 'off',
+        input: 'select',
+        selectArray: 'valMidiList',
+        label: 'Off'
+      },
+      {
+        className: 'w-[19%]',
+        show: true,
+        key: 'default',
+        input: 'checkbox',
+        selectArray: 'valDeftList',
+        label: 'Default'
+      },
+      {
+        className: 'w-[9.5%]',
+        show: true,
+        key: 'delay',
+        input: 'text',
+        selectArray: null,
+        label: 'Delay'
+      },
+      {
+        className: 'w-[9.5%]',
+        show: true,
+        key: 'changeType',
+        input: 'checkbox-switch',
+        selectArray: 'valChngList',
+        label: 'Change'
+      },
+      {
+        className: 'w-[9.5%]',
+        show: true,
+        key: 'ranges',
+        input: 'select-multiple',
+        selectArray: 'artRngsArray',
+        label: 'Ranges'
+      }
+    ]
+  },
+
   {
     title: 'Faders',
     label: 'fadList',
