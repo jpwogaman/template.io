1d491bf9fd354a8588d7ee46569b75ea3bea5f7c
JP Wogaman II <jpwogaman@gmail.com>
2024-12-05 14:03:15 -0500

    feat: extend FileItem type with additional properties for enhanced data structure

--- src-tauri/src/models/fileitem.d.ts
+++ src-tauri/src/models/fileitem.d.ts
@@ -12,3 +12,31 @@ type FileItem = {
   smp_out: string
   color: string
 }
+
+type Prettify<T> = { [K in keyof T]: T[K] } & object
+
+type FileItemExtended = Prettify<
+  FileItem & {
+    full_ranges: Items_Full_Ranges[]
+    art_list_tog: Items_ArtList_Tog[]
+    art_list_tap: Items_ArtList_Tap[]
+    art_layers: Items_Art_Layers[]
+    fad_list: Items_FadList[]
+  }
+>
+
+type FullTrackListForExport = {
+  fileMetaData: {}
+  items: FileItemExtended[]
+}
+
+type FullTrackListWithCounts = {
+  items: FileItem[]
+  counts: {
+    full_ranges: number
+    art_list_tog: number
+    art_list_tap: number
+    art_layers: number
+    fad_list: number
+  }
+}
