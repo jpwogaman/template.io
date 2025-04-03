507dcb4c1360807af99135691000812a632b922f
JP Wogaman II <jpwogaman@gmail.com>
2025-04-03 15:03:22 -0400

    begin implementing logic for avg_delay

--- src-tauri/src/services/items_artlist_service.rs
+++ src-tauri/src/services/items_artlist_service.rs
@@ -3,12 +3,17 @@ use crate::{
   models::{
     items_artlist_tap::{ ItemsArtListTap, ItemsArtListTapRequest },
     items_artlist_tog::{ ItemsArtListTog, ItemsArtListTogRequest },
+    fileitem::{ FileItemRequest },
   },
   schema::{
     items_artlist_tog::dsl as tog_dsl,
     items_artlist_tap::dsl as tap_dsl,
   },
-  services::{ items_artlist_tog_service, items_artlist_tap_service },
+  services::{
+    items_artlist_tog_service,
+    items_artlist_tap_service,
+    fileitem_service,
+  },
 };
 use diesel::prelude::*;
 
@@ -31,7 +36,7 @@ pub fn renumber_all_arts(fileitems_item_id: String) {
         id: format!("{}_AT_{}", fileitems_item_id.clone(), i),
         ..Default::default()
       };
- 
+
       art.update_from(request)
     })
     .collect::<Vec<ItemsArtListTog>>();
@@ -79,4 +84,51 @@ pub fn renumber_all_arts(fileitems_item_id: String) {
   for art in new_artlist_tap {
     items_artlist_tap_service::store_new_art_tap(&art);
   }
+}
+
+pub fn get_avg_delay(fileitems_item_id: String, base_delay: Option<f32>) {
+  let new_base_delay = base_delay.unwrap_or_else(|| {
+    fileitem_service
+      ::get_fileitem(fileitems_item_id.clone())
+      .unwrap().base_delay
+  });
+
+  println!("New base delay: {}", new_base_delay);
+
+  let items_artlist_tog = items_artlist_tog_service::list_items_artlist_tog(
+    fileitems_item_id.clone()
+  );
+
+  let items_artlist_tap = items_artlist_tap_service::list_items_artlist_tap(
+    fileitems_item_id.clone()
+  );
+
+  let mut total_delay = new_base_delay;
+  let mut count = 0;
+
+  for art in items_artlist_tog {
+    total_delay += art.delay as f32;
+    count += 1;
+  }
+
+  for art in items_artlist_tap {
+    total_delay += art.delay as f32;
+    count += 1;
+  }
+
+  let avg_delay = if count > 0 {
+    total_delay / (count as f32)
+  } else {
+    new_base_delay
+  };
+
+  let data = FileItemRequest {
+    id: fileitems_item_id,
+    avg_delay: Some(avg_delay),
+    ..Default::default()
+  };
+
+  println!("Avg delay: {}", avg_delay);
+
+  fileitem_service::update_fileitem(data);
 }
